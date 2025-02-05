9e7f72395ac0671f7efb2fcaed23e5fe7e5a868c
mferrarini <maura_ferrarini@harvard.edu>
2025-02-05 14:39:51 -0500

    adding images to precompile

--- lib/harvard/patterns/gem/engine.rb
+++ lib/harvard/patterns/gem/engine.rb
@@ -2,6 +2,10 @@ module Harvard
   module Patterns
     module Gem
       class Engine < ::Rails::Engine
+        initializer "harvard-patterns-gem.assets.precompile" do |app|
+          app.config.assets.paths << root.join("app", "assets", "images")
+          app.config.assets.precompile += %w[*.png *.jpg *.jpeg *.svg *.gif]
+        end
       end
     end
   end
