d4fa9d59bf154b2c2c9545cb976a75f376bc8b11
mferrarini <maura_ferrarini@harvard.edu>
2025-02-05 15:11:53 -0500

    Update engine.rb

--- lib/harvard/patterns/gem/engine.rb
+++ lib/harvard/patterns/gem/engine.rb
@@ -2,10 +2,6 @@ module Harvard
   module Patterns
     module Gem
       class Engine < ::Rails::Engine
-        initializer "harvard-patterns-gem.assets.precompile" do |app|
-          app.config.assets.paths << root.join("app", "assets", "images")
-          app.config.assets.precompile += %w[*.png *.jpg *.jpeg *.svg *.gif]
-        end
       end
     end
   end
